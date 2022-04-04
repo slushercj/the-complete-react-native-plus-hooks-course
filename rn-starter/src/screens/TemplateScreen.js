8639e71eb123fd2b19556824aa8009a156b93228
cslusher <slusher.cj@gmail.com>
2022-04-03 22:40:54 -0700

    Section 3: List Building - With Style!

---
+++ rn-starter/src/screens/TemplateScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const TemplateScreen = () => {
+  return <Text>Template Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TemplateScreen;
